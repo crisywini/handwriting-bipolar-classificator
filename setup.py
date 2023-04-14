6283e3cf6af048786afade6b79f47adcb08f4844
Cristian Sánchez <harmaharcri@hotmail.com>
2023-04-13 23:02:44 -0500

    Init project with cookiecutter for data science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project analyzes data from people with and without bipolar disorder',
+    author='Cristian Giovanny Sanchez Pineda',
+    license='MIT',
+)
